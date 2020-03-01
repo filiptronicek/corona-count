467535b8a01ac85b3b3ea6639ae830b7bc8db137
Filip Troníček <filip.tronicek@seznam.cz>
2020-03-01 21:21:35 +0100

    Animating text

--- js/script.js
+++ js/script.js
@@ -2,9 +2,32 @@ const url = "https://coronavirus-tracker-api.herokuapp.com/confirmed";
 const tptDiv = document.getElementById("num");
 const percent = document.getElementById("percentage");
 
-const population = 7778973427;
+const population = 7794798739;
 
-fetch(url)
-  .then(response => response.json())
-  .then(resp => tptDiv.innerText = resp.latest)
-  .then(resp => percent.innerText = `${(100*(Number(resp) / population)).toFixed(6)}%`);
+let commaDigits = 4;
+let percentage;
+
+percent.onclick = function() {
+  if(commaDigits > 8) {
+    commaDigits = 2;
+  } else {
+    commaDigits ++;
+  }
+  percent.innerText = `${percentage.toFixed(
+    commaDigits
+  )}%`;
+};
+
+function getData(commaLvl = 4) {
+  fetch(url)
+    .then(response => response.json())
+    .then(resp => (tptDiv.innerText = resp.latest))
+    .then(resp => {
+      percent.innerText = `${(100 * (Number(resp) / population)).toFixed(
+        commaLvl
+      )}%`;
+      percentage = 100 * (Number(resp) / population);
+    });
+}
+
+getData();
