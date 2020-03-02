d7a58b113d294b6adeb827b4d438aeb6b7a054bd
Filip Troníček <filip.tronicek@seznam.cz>
2020-03-02 18:24:08 +0100

    Interactive number

--- js/most.js
+++ js/most.js
@@ -1,37 +1,54 @@
 const url = "https://coronavirus-tracker-api.herokuapp.com/confirmed";
 const table = document.getElementById("countryTables");
 
-numOfPlacesToShow = 10;
+numOfPlacesToShow = 5;
 
 let sorted = [];
 
-fetch(url)
-  .then(response => response.json())
-  .then(resp => (resp = resp.locations))
-  .then(resp => {
-    let objSorted = resp
-      .sort(function(a, b) {
-        return a.latest - b.latest;
-      })
-      .reverse();
-    let rank = 0;
-    for (let instance in objSorted) {
-      rank++;
-      let listStr = "";
-
-      if (objSorted[instance].province !== "") {
-        listStr = `${objSorted[instance].province}, ${objSorted[instance].country}`;
-      } else {
-        listStr = `${objSorted[instance].country}`;
-      }
-      if (rank < numOfPlacesToShow) {
-        table.innerHTML += `
-        <tr>
-            <td> ${listStr} </td>
-            <td> ${objSorted[instance].latest} </td>
-        </tr>
-        `;
-      }
-      console.log(instance);
+function changeNum(numToShow = numOfPlacesToShow) {
+  function changeNumGen(textNum, domject) {
+    const realNum = Number(textNum);
+    if (realNum < 100) {
+      changeNum(realNum + 5);
+      domject.innerText = realNum + 5;
+    } else {
+        changeNum(numOfPlacesToShow);
+        domject.innerText = numOfPlacesToShow.toString();
     }
-  });
+  }
+  console.log("rendering " + numToShow);
+  fetch(url)
+    .then(response => response.json())
+    .then(resp => (resp = resp.locations))
+    .then(resp => {
+      table.innerHTML = "";
+
+      let objSorted = resp
+        .sort(function(a, b) {
+          return a.latest - b.latest;
+        })
+        .reverse();
+      let rank = 0;
+      for (let instance in objSorted) {
+        rank++;
+        let listStr = "";
+
+        if (objSorted[instance].province !== "") {
+          listStr = `${objSorted[instance].province}, ${objSorted[instance].country}`;
+        } else {
+          listStr = `${objSorted[instance].country}`;
+        }
+        if (rank < numToShow + 1) {
+          table.innerHTML += `
+              <tr>
+                  <td> ${listStr} </td>
+                  <td> ${objSorted[instance].latest} </td>
+              </tr>
+              `;
+        }
+      }
+      return objSorted.length;
+    });
+  changeNum.gen = changeNumGen;
+}
+changeNum();
